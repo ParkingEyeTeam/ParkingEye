5d3aaa6fe55cd383bf2c693b0835272528676ed2
Neroec <roman.nikanorov@mail.ru>
2022-04-07 01:51:58 +0300

    Модуль карты: добавил определение расстояния на основе OpenStreetMap, новые зависимости в окружении

--- server/map/map.py
+++ server/map/map.py
@@ -1,8 +1,64 @@
 import math
 import random
+import osmnx as ox
+import networkx as nx
 
 
 class Map:
+    mock_cameras = [
+        {'camera_id': 1, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.80843, 34.319], 'parking_places': []},
+        {'camera_id': 2, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.81034, 34.32836], 'parking_places': []},
+        {'camera_id': 3, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.80943, 34.33114], 'parking_places': []},
+        {'camera_id': 4, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.80797, 34.33023], 'parking_places': []},
+        {'camera_id': 5, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.80657, 34.3396], 'parking_places': []},
+        {'camera_id': 6, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.8022, 34.32632], 'parking_places': []},
+        {'camera_id': 7, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.8015, 34.32481], 'parking_places': []},
+        {'camera_id': 8, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.80065, 34.33137], 'parking_places': []},
+        {'camera_id': 9, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.79444, 34.37681], 'parking_places': []},
+        {'camera_id': 10, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.7895, 34.36859], 'parking_places': []},
+        {'camera_id': 11, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.79047, 34.37184], 'parking_places': []},
+        {'camera_id': 12, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.78094, 34.32517], 'parking_places': []},
+        {'camera_id': 13, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.76542, 34.3142], 'parking_places': []},
+        {'camera_id': 14, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.75969, 34.31625], 'parking_places': []},
+        {'camera_id': 15, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.75903, 34.35843], 'parking_places': []},
+        {'camera_id': 16, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.76131, 34.36878], 'parking_places': []},
+        {'camera_id': 17, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.7681, 34.38785], 'parking_places': []},
+        {'camera_id': 18, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.76248, 34.41948], 'parking_places': []},
+        {'camera_id': 19, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.78496, 34.34497], 'parking_places': []},
+        {'camera_id': 20, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
+         'coords': [61.78749, 34.38096], 'parking_places': []}
+    ]
+    __graph = None
+
+    @staticmethod
+    def init(center=(61.787439, 34.354423), dist=5000):
+        """
+        Инициализирует класс, загружает карту местности
+        :param center: точка центра карты
+        :param dist: расстояние от точки
+        :return: None
+        """
+        Map.__graph = ox.graph_from_point(center, dist=dist, simplify=True, network_type='all')
+
     @staticmethod
     def generate_route_link(point_a, point_b, site='yandex'):
         """
@@ -44,37 +100,49 @@ class Map:
         return link
 
     @staticmethod
-    def get_point_point_distant(point_a, point_b):
+    def get_point_point_distant(point_a, point_b, method='dijkstra'):
         """
         Вычисляет расстояние между двумя точками
         :param point_a: первая точка (lat, lng)
         :param point_b: вторая точка (lat, lng)
+        :param method: способ расчета расстояния ['dijkstra', 'euclid']
         :return: расстояние между точками
         """
         lat1, lng1 = point_a
         lat2, lng2 = point_b
-        lat_dif, lng_dif = lat1 - lat2, lng1 - lng2
-        return math.sqrt(lat_dif * lat_dif + lng_dif * lng_dif)
+        if method == 'dijkstra':
+            source = ox.nearest_nodes(Map.__graph, lng1, lat1)
+            target = ox.nearest_nodes(Map.__graph, lng2, lat2)
+            distance = nx.shortest_path_length(Map.__graph, source, target, weight='length')
+        elif method == 'euclid':
+            lat_dif, lng_dif = lat1 - lat2, lng1 - lng2
+            distance = math.sqrt(lat_dif * lat_dif + lng_dif * lng_dif)
+        else:
+            raise ValueError(f'Unknown method={method}')
+
+        return distance
 
     @staticmethod
-    def get_camera_point_distant(camera, point):
+    def get_camera_point_distant(camera, point, method='dijkstra'):
         """
         Вычисляет расстояние между камерой и точкой
         :param camera: камера
         :param point: точка (lat, lng)
+        :param method: способ расчета расстояния ['dijkstra', 'euclid']
         :return: расстояние между камерой и точкой
         """
         lat, lng = camera['coords']
-        return Map.get_point_point_distant((lat, lng), point)
+        return Map.get_point_point_distant((lat, lng), point, method)
 
     @staticmethod
-    def __quicksort(cameras, point, fst, lst):
+    def __quicksort(cameras, point, fst, lst, method='dijkstra'):
         """
         Сортировка Хоара для камер по расстоянию до точки
         :param cameras: список камер
         :param point: точка
         :param fst: начальный индекс сортировки
         :param lst: конечный индекс сортировки
+        :param method: способ расчета расстояния ['dijkstra', 'euclid']
         :return: отсортированный лист камер
         """
         if fst >= lst:
@@ -82,83 +150,43 @@ class Map:
 
         i, j = fst, lst
         camera = cameras[random.randint(fst, lst)]
-        pivot = Map.get_camera_point_distant(camera, point)
+        pivot = Map.get_camera_point_distant(camera, point, method)
 
         while i <= j:
-            while Map.get_camera_point_distant(cameras[i], point) < pivot:
+            while Map.get_camera_point_distant(cameras[i], point, method) < pivot:
                 i += 1
-            while Map.get_camera_point_distant(cameras[j], point) > pivot:
+            while Map.get_camera_point_distant(cameras[j], point, method) > pivot:
                 j -= 1
             if i <= j:
                 cameras[i], cameras[j] = cameras[j], cameras[i]
                 i, j = i + 1, j - 1
-        Map.__quicksort(cameras, point, fst, j)
-        Map.__quicksort(cameras, point, i, lst)
+        Map.__quicksort(cameras, point, fst, j, method)
+        Map.__quicksort(cameras, point, i, lst, method)
         return cameras
 
     @staticmethod
-    def sort_cameras(cameras, point):
+    def sort_cameras(cameras, point, method='dijkstra'):
         """
         Возвращает отсортированный по возрастанию расстояний до точки
         список камер
         :param cameras: начальный список камер
         :param point: точка
+        :param method: способ расчета расстояния ['dijkstra', 'euclid']
         :return: отсортированный лист камер
         """
         length = len(cameras)
         if length == 0:
             return cameras
 
-        return Map.__quicksort(cameras, point, 0, length - 1)
+        return Map.__quicksort(cameras, point, 0, length - 1, method)
 
     @staticmethod
-    def sort_mock_cameras(point):
+    def sort_mock_cameras(point, method='dijkstra'):
         """
         Возвращает отсортированный по возрастанию расстояний до точки
         список фиксированных камер
         :param point: точка
+        :param method: способ расчета расстояния ['dijkstra', 'euclid']
         :return: отсортированный лист камер
         """
-        mock_cameras = [
-            {'camera_id': 1, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.80843, 34.319], 'parking_places': []},
-            {'camera_id': 2, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.81034, 34.32836], 'parking_places': []},
-            {'camera_id': 3, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.80943, 34.33114], 'parking_places': []},
-            {'camera_id': 4, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.80797, 34.33023], 'parking_places': []},
-            {'camera_id': 5, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.80657, 34.3396], 'parking_places': []},
-            {'camera_id': 6, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.8022, 34.32632], 'parking_places': []},
-            {'camera_id': 7, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.8015, 34.32481], 'parking_places': []},
-            {'camera_id': 8, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.80065, 34.33137], 'parking_places': []},
-            {'camera_id': 9, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.79444, 34.37681], 'parking_places': []},
-            {'camera_id': 10, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.7895, 34.36859], 'parking_places': []},
-            {'camera_id': 11, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.79047, 34.37184], 'parking_places': []},
-            {'camera_id': 12, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.78094, 34.32517], 'parking_places': []},
-            {'camera_id': 13, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.76542, 34.3142], 'parking_places': []},
-            {'camera_id': 14, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.75969, 34.31625], 'parking_places': []},
-            {'camera_id': 15, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.75903, 34.35843], 'parking_places': []},
-            {'camera_id': 16, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.76131, 34.36878], 'parking_places': []},
-            {'camera_id': 17, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.7681, 34.38785], 'parking_places': []},
-            {'camera_id': 18, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.76248, 34.41948], 'parking_places': []},
-            {'camera_id': 19, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.78496, 34.34497], 'parking_places': []},
-            {'camera_id': 20, 'camera_url': 'https://s1.moidom-stream.ru/s/public/0000000088.m3u8',
-             'coords': [61.78749, 34.38096], 'parking_places': []}
-        ]
-        return Map.sort_cameras(mock_cameras, point)
+        return Map.sort_cameras(Map.mock_cameras, point, method)
