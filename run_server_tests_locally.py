584adffdc6f33518ea733e02fea9c83b6fad2744
fragers <igorsim35@gmail.com>
2022-05-11 21:32:50 +0300

    удаление папки research

--- run_server_tests_locally.py
+++ run_server_tests_locally.py
@@ -1,7 +1,10 @@
 import os
 import argparse
 os.environ["MONGO_DB"] = 'test_server_db'
+IGOR = True
 
+if IGOR:
+    os.environ["MONGO_HOST"] = 'localhost:27020'
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--test_group', required=False, default=None,
