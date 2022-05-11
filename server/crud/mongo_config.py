abc6eace30fb3a7809028970847bd1ceba67f665
Neroec <roman.nikanorov@mail.ru>
2022-05-11 18:54:58 +0300

    Название БД теперь берется из конфига

--- server/crud/mongo_config.py
+++ server/crud/mongo_config.py
@@ -5,7 +5,8 @@ load_dotenv()
 
 mongo_config = {
     'host': os.getenv('MONGO_HOST'),
-    'db': 'parking_eye'
+    'db': os.getenv('MONGO_DB')
+    # 'db': 'parking_eye'
 }
 
 mongo_connection_string = f"mongodb://{mongo_config['host']}"
